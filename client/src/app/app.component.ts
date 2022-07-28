70e434e3238281e89a64e2334be5e217fbbfb91e
Steve Blythe <blythesr@gmail.com>
2022-07-28 14:31:38 +0100

    Http get request from Angular for Users

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'CCG Trader Hub';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get("https://localhost:5000/api/users").subscribe(
+      response => this.users = response,
+      error => console.log(error),
+    )
+  }
 }
