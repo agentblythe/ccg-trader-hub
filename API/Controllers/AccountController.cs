54beb6b8fadba7353c82aa7adc8f75cd521d8a0f
Steve Blythe <blythesr@gmail.com>
2022-09-14 18:44:38 +0100

    Creating a User DTO and returning the token

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,14 +12,16 @@ namespace API.Controllers
     public class AccountController : BaseAPIController
     {
         private readonly DataContext _context;
+        private readonly ITokenService _tokenService;
 
-        public AccountController(DataContext context)
+        public AccountController(DataContext context, ITokenService tokenService)
         {
+            _tokenService = tokenService;
             _context = context;
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDTO) {
+        public async Task<ActionResult<UserDTO>> Register(RegisterDTO registerDTO) {
 
             if (await UserExists(registerDTO.Username)) return BadRequest("Username is already taken");
 
@@ -34,11 +37,14 @@ namespace API.Controllers
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
-            return user;
+            return new UserDTO {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user),
+            };
         }
 
         [HttpPost("login")]
-        public async Task<ActionResult<AppUser>> Login(LoginDTO loginDTO) {
+        public async Task<ActionResult<UserDTO>> Login(LoginDTO loginDTO) {
             var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDTO.Username);
 
             if (user == null) return Unauthorized("Inavlid Username");
@@ -51,7 +57,10 @@ namespace API.Controllers
                 if (computedHash[i] != user.PasswordHash![i]) return Unauthorized("Invalid Password");
             }
 
-            return user;
+            return new UserDTO {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user),
+            };
         }
 
         private async Task<bool> UserExists(string username) {
